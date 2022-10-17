835863c4f9f1317642c0d547ed1f951ccd470a2f
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-10-18 00:10:23 +0400

    Add user pfp property

---
+++ EduPlay/EduPlay.WebAPI/Models/UserGameRecords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace EduPlay.WebAPI.Models
+{
+    public partial class UserGameRecords
+    {
+        public Guid Id { get; set; }
+        public Guid GameId { get; set; }
+        public int Score { get; set; }
+        public string UserId { get; set; }
+
+        public virtual Games Game { get; set; }
+        public virtual AspNetUsers User { get; set; }
+    }
+}
