9bd80358e77a403bbc83b22a3d0aeac17baaaa58
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-10-19 13:00:40 +0400

    Add DTO models

---
+++ EduPlay/EduPlay.BLL/Models/GameDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduPlay.BLL.Models
+{
+    public class GameDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid ThemeId { get; set; }
+        public int AgeLimit { get; set; }
+        public int MaxScore { get; set; }
+    }
+}
