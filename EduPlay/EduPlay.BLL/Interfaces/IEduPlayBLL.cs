9903ae0f2b67688fa886395973c21194ebad504f
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-10-19 13:01:16 +0400

    Add BLL interface

---
+++ EduPlay/EduPlay.BLL/Interfaces/IEduPlayBLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EduPlay.BLL.Models;
+
+namespace EduPlay.BLL.Interfaces
+{
+    public interface IEduPlayBLL
+    {
+        public void AddUserGameRecord(UserGameRecordDTO gameRecordDTO);
+        public void UpdateUserGameRecord(UserGameRecordDTO newGameRecordDTO);
+        public void RemoveGameRecordDTO(UserGameRecordDTO gameRecordDTO);
+        public List<UserGameRecordDTO> GetUserGameRecordsByUserId(string id);
+        public List<UserGameRecordDTO> GetUserGameRecordsByGameId(Guid id);
+        public List<GameDTO> GetAllGames();
+        public List<GameDTO> GetGamesByThemeId(Guid id);
+        public List<GameDTO> GetGamesByDifficultyId(Guid id);
+        public GameDTO GetGameById(Guid id);
+        public List<UserDTO> GetAllUsers();
+        public UserDTO GetUserById(string id);
+        public UserDTO GetUserByEmail(string email);
+        public void UpdateUser(UserDTO user);
+    }
+}
