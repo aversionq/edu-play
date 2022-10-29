1127f73a86812c64eabe10b0984147d36a64e592
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-10-29 13:09:34 +0400

    Add Leaderboard controller

---
+++ EduPlay/EduPlay.WebAPI/Controllers/LeaderboardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EduPlay.BLL.Interfaces;
+using EduPlay.BLL.Models;
+using EduPlay.Dependencies;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+
+namespace EduPlay.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaderboardController : ControllerBase
+    {
+        private readonly IEduPlayBLL _bll;
+
+        public LeaderboardController()
+        {
+            _bll = DependencyResolver.Instance.EduPlayBLL;
+        }
+
+        [HttpGet]
+        [Route("getUserGameRecordsByGameId")]
+        public async Task<List<UserGameRecordDTO>> GetUserGameRecordsByGameId(Guid id)
+        {
+            return await _bll.GetUserGameRecordsByGameId(id);
+        }
+    }
+}
