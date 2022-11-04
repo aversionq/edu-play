aa04a00516f5a9bcba280ca6d6b963ce30ee7067
LAPTOP-S9P9KUJ7\arskz <70411996+aversionq@users.noreply.github.com>
2022-11-04 22:30:56 +0400

    Add theme and difficulty DTO

---
+++ EduPlay/EduPlay.BLL/Models/DifficultyDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduPlay.BLL.Models
+{
+    public class DifficultyDTO
+    {
+        public Guid Id { get; set; }
+        public int Value { get; set; }
+    }
+}
